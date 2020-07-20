c240975115a67926285332fd1e4cc6dcf918286e
Jessica Blankemeier <jessicablankemeier@gmail.com>
2020-07-20 08:51:28 -0700

    adding database schema and seeds

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('BBQ Bacon Cheeseburger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mushroom Burger', FALSE);
